58d91583072944b8fcb4d59fe212d8b39a682ffa
Samuel Adewole <samueladewole15@gmail.com>
2019-08-11 18:45:18 +0100

    designed the home and details page

---
+++ src/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+  state = {
+    products: [],
+    detailProduct: detailProduct
+  };
+
+  componentDidMount() {
+    this.setProduct();
+  }
+
+  setProduct = () => {
+    let products = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+
+    this.setState(() => {
+      return { products };
+    });
+  };
+
+  handleDetail = () => {
+    console.log('hello from details');
+  };
+
+  addToCart = id => {
+    console.log(`Hello from add to cart ${id}`);
+  };
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {' '}
+        {this.props.children}{' '}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
