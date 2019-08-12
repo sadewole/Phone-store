a2dd07943e1029b14b8e1ca9f1e3d7664866dc41
Samuel Adewole <samueladewole15@gmail.com>
2019-08-12 09:10:30 +0100

    added decrement functionality

--- src/context.js
+++ src/context.js
@@ -99,11 +99,32 @@ class ProductProvider extends Component {
   };
 
   decrement = id => {
-    console.log('thisis dec mtd');
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart]
+          };
+        },
+        () => {
+          this.addTotal();
+        }
+      );
+    }
   };
 
   removeItem = id => {
-    let tempProducts = [...this.this.state.products];
+    let tempProducts = [...this.state.products];
     let tempCart = [...this.state.cart];
 
     tempCart = tempCart.filter(item => item.id !== id);
